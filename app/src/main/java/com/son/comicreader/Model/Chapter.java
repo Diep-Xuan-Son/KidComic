9e90494b2f2f95f580eb658f4819ab820383e906
Diep-Xuan-Son <82936265+Diep-Xuan-Son@users.noreply.github.com>
2021-11-23 17:51:59 +0700

    upload

---
+++ app/src/main/java/com/son/comicreader/Model/Chapter.java
@@ -0,0 +1,11 @@
+package com.son.comicreader.Model;
+
+import java.util.List;
+
+public class Chapter {
+    public String Name;
+    public List<String> Links;
+
+    public Chapter() {
+    }
+}
---
+++ app/src/main/java/com/son/comicreader/Model/Comic.java
@@ -0,0 +1,13 @@
+package com.son.comicreader.Model;
+
+import java.util.List;
+
+public class Comic {
+    public String Name;
+    public String Image;
+    public String Category;
+    public List<Chapter> Chapter;
+
+    public Comic() {
+    }
+}
